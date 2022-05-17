ac88061930c530c580b5cb9952b7c4fca7b64cd9
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-17 21:45:13 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,19 @@ import { isDark, toggleColorScheme } from './theme';
 <template>
   <div>
     <div class="dark:bg-dark-800">
-      <div class="h-[60px] flex align-center px-4">
-        <div class="flex justify-between w-full">
-          <button class="outline-none" @click="toggleColorScheme()">
-            <div class="text-blue-500 dark:text-yellow-300">
-              <MoonStarsIcon v-if="!isDark" />
-              <SunIcon v-if="isDark" />
-            </div>
-          </button>
+      <div class="fixed w-full top-0 h-[60px]">
+        <div class="h-[60px] flex align-center px-4">
+          <div class="flex justify-between w-full">
+            <button class="outline-none" @click="toggleColorScheme()">
+              <div class="text-blue-500 dark:text-yellow-300">
+                <MoonStarsIcon v-if="!isDark" />
+                <SunIcon v-else />
+              </div>
+            </button>
+          </div>
         </div>
       </div>
-      <div class="grid place-items-center h-[calc(100vh-60px)]">
+      <div class="grid place-items-center h-[calc(100vh)]">
         <div class="flex flex-col items-center text-center">
           <router-view />
         </div>
