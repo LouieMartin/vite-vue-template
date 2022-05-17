09d04906600f19d5c27aa2be4ad255c972cf5199
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-17 21:45:58 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,7 +12,7 @@ const enter = () => {
 <template dark>
   <span class="text-xl dark:text-white font-mono">Vite Vue Template</span>
   <span class="text-gray-300 dark:text-dark-100">A Vite.js and Vue.js template for minimalists.</span>
-  <input @keydown.enter="enter()" v-model="name" type="text" class="mt-10 border-light-900 border py-2 px-5 rounded-md outline-none text-center dark:border-dark-300 dark:text-white dark:placeholder-dark-100 bg-transparent" placeholder="What is your name?" />
+  <input @keydown.enter="enter()" v-model="name" type="text" class="mt-10 border-light-900 border py-2 px-2 rounded-md outline-none text-center dark:border-dark-300 dark:text-white dark:placeholder-dark-100 bg-transparent" placeholder="What is your name?" />
   <input :disabled="name === ''" @click="enter()" type="submit" class="mt-3 px-5 py-1 disabled:bg-dark-900 dark:text-white dark:bg-dark-500 rounded font-semibold" value="Enter" />
 </template>
 
