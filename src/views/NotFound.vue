148e96119b0e0d7f2b3f68276721dcafb5bac039
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-18 10:56:01 +0800

    Create NotFound.vue

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="flex dark:text-white">
+        <span class="pr-4 mr-4 border-r text-xl">404</span>
+        <span>Not Found</span>
+    </div>
+</template>
+
+<style>
+</style>
