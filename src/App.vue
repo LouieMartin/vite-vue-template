2bfd5aa6a25fd8d0f4449c2158458c289723e2d7
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-18 10:36:41 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <script setup lang="ts">
-import { MoonStarsIcon, SunIcon } from 'vue-tabler-icons';
-import { isDark, toggleColorScheme } from './theme';
+import { MoonStarsIcon, Error404Icon, FriendsIcon, HomeIcon, SunIcon } from 'vue-tabler-icons';
+import { toggleColorScheme, isDark } from './theme';
 </script>
 
 <template>
   <div>
     <div class="dark:bg-dark-800">
       <div class="fixed w-full top-0 h-[60px]">
-        <div class="h-[60px] flex align-center px-4">
-          <div class="flex justify-between w-full">
+        <div class="h-full px-4">
+          <div class="flex items-center justify-between h-full w-full">
+            <div class="flex">
+              <div class="dark:text-white pr-1">
+                <HomeIcon v-if="$route.name === 'Home'" />
+                <FriendsIcon v-else-if="$route.name === 'Welcome'" />
+                <Error404Icon v-else />
+              </div>
+              <span class="text-md dark:text-white font-bold">{{ $route.name ? $route.name : 'Not Found' }}</span>
+            </div>
             <button class="outline-none" @click="toggleColorScheme()">
               <div class="text-blue-500 dark:text-yellow-300">
                 <MoonStarsIcon v-if="!isDark" />
