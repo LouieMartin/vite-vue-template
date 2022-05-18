555e247bdc6a0821608660e0ebd19144fd97bc9d
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-18 10:55:54 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
-import { createWebHistory,  createRouter } from "vue-router";
-import Welcome from "../views/Welcome.vue";
-import Home from "../views/Home.vue";
+import { createWebHistory,  createRouter } from 'vue-router';
+import NotFound from '../views/NotFound.vue';
+import Welcome from '../views/Welcome.vue';
+import Home from '../views/Home.vue';
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/welcome/:name", name: 'Welcome', component: Welcome },
-    { path: "/", name: 'Home', component: Home },
+    { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+    { path: '/welcome/:name', name: 'Welcome', component: Welcome },
+    { path: '/', name: 'Home', component: Home },
   ],
 });
