cf2d81c55102d1d6591bc64d92dfdd7875e78d68
DoNotHaveAnIdeaForAUsername <louie.decierdo@gmail.com>
2022-05-18 10:36:15 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHistory,  createRouter } from "vue-router";
 import Welcome from "../views/Welcome.vue";
 import Home from "../views/Home.vue";
 
 export const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: "/welcome/:name", component: Welcome },
-    { path: "/", component: Home },
+    { path: "/welcome/:name", name: 'Welcome', component: Welcome },
+    { path: "/", name: 'Home', component: Home },
   ],
 });
